c283143b5bb544ba891b7fcdc23a8e930c70de7e
MARCO-SIR <110541377+MARCO-SIR@users.noreply.github.com>
2022-08-15 22:03:10 +0530

    Update commands.py

--- bot/plugins/commands.py
+++ bot/plugins/commands.py
@@ -28,14 +28,14 @@ async def start(bot, update):
             await update.reply_cached_media(
                 file_id,
                 quote=True,
-                caption = caption,
+                caption = f"{file_name} \n @CumiyaBot,
                 parse_mode="html",
                 reply_markup=InlineKeyboardMarkup(
                     [
                         [
                             InlineKeyboardButton
                                 (
-                                    'Developers', url="https://t.me/CrazyBotsz"
+                                    '✅CONTACT ME✅', url="https://t.me/ffaccountexchangeleadersir"
                                 )
                         ]
                     ]
