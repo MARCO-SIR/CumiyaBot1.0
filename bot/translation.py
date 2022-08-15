ab883fe4690875da77cb671b5dc59f9da7846ffc
MARCO-SIR <110541377+MARCO-SIR@users.noreply.github.com>
2022-08-15 21:52:39 +0530

    Update translation.py

--- bot/translation.py
+++ bot/translation.py
@@ -4,13 +4,13 @@
 
 class Translation(object):
     
-    START_TEXT = """<b>Hey {}!!</b>
-<i>Am Just A Advance Auto Filter Bot....😉
+    START_TEXT = """<b>Hey {} I AM SUZUA !!</b>
+<i>YOU CAN CHECK HIM AT wa.me/+918257866512
 
-Just Add Me To Your Group And Channel And Connect Them And See My Pevers 🔥🔥😝
+I work for marco as a assistant 
 
 For More Details Click Help Button Below..
-@CrazyBotsz
+@CumiyaBot
 </i>"""    
     
     HELP_TEXT = """
@@ -43,16 +43,16 @@ For More Details Click Help Button Below..
             
             -> <code>Status</code> - Button Will Shows The Stats Of Your Channel
             
-@CrazyBotsz
+
 """
     
     ABOUT_TEXT = """<b>➥ Name</b> : <code> Auto Filter Bot</code>
     
-<b>➥ Creator</b> : <b><i><a href="https://t.me/AlbertEinstein_TG">AlbertEinstein_TG</a></i></b>
+<b>➥ Creator</b> : <b><i><a href="https://t.me/horimatsu">Marco</a></i></b>
 
 <b>➥ Language</b> : <code>Python3</code>
 
 <b>➥ Library</b> : <i><a href="https://docs.pyrogram.org">Pyrogram Asyncio 1.13.0 </a></i>
 
-<b>➥ Source Code</b> : <i><a href="https://github.com/AlbertEinsteinTG/Adv-Auto-Filter-Bot">Click Me</a></i>
+<b>➥ Source Code</b> : <i><a href="https://github.com/MARCO-SIR/CumiyaBot1.0">Click Me</a></i>
 """
